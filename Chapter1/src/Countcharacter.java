3bd8c7aaf4e5ee3b08863182808b090f4a44e309
jihyun0385 <lucyjihyun0385@gmail.com>
2023-08-13 22:20:24 +0900

    countchar, space

--- Chapter1/src/Countcharacter.java
+++ Chapter1/src/Countcharacter.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Countcharacter {
 
@@ -6,18 +7,27 @@ public class Countcharacter {
 
 		String example = "20230810 자고싶다 정말 abcdef ";
 
-		// 단순 총 개수 세기 
-		int count = example.length();
-		System.out.println(count);
-		
-		int text, loweng, upeng =0;
-		
-		// 숫자 0-9제외하고는 다 공백만들어서 카운트 
-			String num = example.replaceAll("[^0-9]","");
-            System.out.println(num.length());
+		// 공백포함 총 개수 세기
+		int resultcount = example.length();
+		System.out.println("총 글자수는" + resultcount + " 자리입니다.(공백포함)");
+
+		// 공백제외 총 문자수 세기
+		int blank = 0;
+		for (int a = 0; a < example.length(); a++) {
+			char textpoint = example.charAt(a);
+			if (textpoint == ' ') {
+				blank++;
+			}
 		}
-	
-	
+		int nospaceresult = example.length() - blank;
+		System.out.println("공백 제외한 총 글자수는 " + nospaceresult + " 입니다");
+
+		// 숫자 0-9제외하고는 다 공백만들어서 숫자만 카운트
+		String num = example.replaceAll("[^0-9]", "");
+		System.out.println("숫자는 " + num.length() + " 자리입니다");
+
+		// 문자만 카운트
+		String countchar = example.replaceAll("[^a-zA-Zㄱ-힣]", "");
+		System.out.println("문자는 " + countchar.length() + "자리입니다.");
 	}
-
-
+}
